358567c4fefe43f16f3f73e472d5e87a043b528e
Kirk Peters <kirkanthonypeters@gmail.com>
2025-07-19 18:42:35 -0400

    feat: added admin dashboard for data analytics

--- backend/models/Article.js
+++ backend/models/Article.js
@@ -5,6 +5,7 @@ const ArticleSchema = new mongoose.Schema(
     title: { type: String, required: true },
     content: { type: String },
     category: { type: String },
+    subcategory: { type: String },
   },
   { timestamps: true }
 );
